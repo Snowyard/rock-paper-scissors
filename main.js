5f5df893407b25a764aa2b89419c52b76b68f460
brunobruno <brett.bruno@yahoo.ca>
2022-04-05 16:53:52 -0700

    added game() function that loops a single round of rock paper scissor until the gameCount of 5 has been reached where the program stops.
    PROBLEM: the loop constantly calls singleRound() function and the user as a result gets continuously prompted to enter his turn 5 times, before the program executes.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function playerSelection() {
     let playersChoice;
     reattempt = true;
     while(reattempt) {
-        playersChoice = prompt('Choose rock, paper, or scissors: ');
+        playersChoice = prompt('Choose rock, paper, or scissors: ').toLowerCase();
         if(playersChoice=='rock') {
             reattempt = false;
         }
@@ -25,34 +25,51 @@ function playerSelection() {
     }
     return playersChoice;
 }
-
+// declaring playerScore and computerScore
+let playerScore = 0;
+let computerScore = 0;
+let gameCount = 0;
 // this function will play a single round of RPS using 2 parameters: 
 // computerSelection and playerSelection
-
 function singleRound(computer, player) {
     let playerChoice = document.querySelector('.playerChoice');
     let computerChoice = document.querySelector('.computerChoice');
     let winner = document.querySelector('.winner');
     let score = document.querySelector('.score');
-
-    let playerScore = 0;
-    let computerScore = 0;
+    let gameNumber = document.querySelector('.gameNumber');
 
     playerChoice.textContent = "You chose " + player + "!";
     computerChoice.textContent = "Your computer chose " + computer + "!";
     // if its a draw
     if(computer==player) {
+        gameCount += 1;
+        gameNumber.textContent = 'Game: ' + gameCount;
         winner.textContent = "It's a tie!";
-        score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore; 
-    }
-    // if computer has rock and you have scissors
-    else if(computer=='rock' && player=='scissors') {
-        winner.textContent = 'The Computer Won! '
-        computerScore += 1;
         score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore;
     }
-    //
-    else if(computer=='paper' && player=)
+    // if computer has a win condition, will +1 to its score
+    else if((computer=='rock' && player=='scissors')
+            || (computer=='paper' && player=='rock')
+            || (computer=='scissors' && player=='paper')) {
+        gameCount += 1;
+        gameNumber.textContent = 'Game: ' + gameCount;
+        winner.textContent = 'The Computer Won! '
+        computerScore += 1;
+        score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore; 
+    }
+    else {
+        gameCount += 1;
+        gameNumber.textContent = 'Game: ' + gameCount;
+        winner.textContent = 'You have won!';
+        playerScore += 1;
+        score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore; 
+    }
 }
 
-singleRound(computerSelection(), playerSelection());
+function game() {
+    while(gameCount!=5) {
+        singleRound(computerSelection(), playerSelection());
+    }
+}
+
+game();
