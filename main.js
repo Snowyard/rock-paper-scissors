3ea1a168d6167b6b923fd945904f5b30556cf561
brunobruno <brett.bruno@yahoo.ca>
2022-04-05 20:25:12 -0700

    fixed prompt flood, enabled user to play a round of 5 from by putting game(); into console

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function playerSelection() {
 let playerScore = 0;
 let computerScore = 0;
 let gameCount = 0;
+let totalScore = 0;
 // this function will play a single round of RPS using 2 parameters: 
 // computerSelection and playerSelection
 function singleRound(computer, player) {
@@ -46,6 +47,8 @@ function singleRound(computer, player) {
         gameNumber.textContent = 'Game: ' + gameCount;
         winner.textContent = "It's a tie!";
         score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore;
+        totalScore = playerScore + computerScore;
+        return player + " ties " + computer + "! Human: " + playerScore + " Computer: " + computerScore;
     }
     // if computer has a win condition, will +1 to its score
     else if((computer=='rock' && player=='scissors')
@@ -56,6 +59,8 @@ function singleRound(computer, player) {
         winner.textContent = 'The Computer Won! '
         computerScore += 1;
         score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore; 
+        totalScore = playerScore + computerScore;
+        return computer + " beats " + player + "! Human: " + playerScore + " Computer: " + computerScore;
     }
     else {
         gameCount += 1;
@@ -63,13 +68,15 @@ function singleRound(computer, player) {
         winner.textContent = 'You have won!';
         playerScore += 1;
         score.textContent = "Your score: " + playerScore + "\n" + "Computer score: " + computerScore; 
+        totalScore = playerScore + computerScore;
+        return player + " beats " + computer + "! Human: " + playerScore + " Computer: " + computerScore;
     }
 }
 
 function game() {
-    while(gameCount!=5) {
-        singleRound(computerSelection(), playerSelection());
+    while(totalScore<5) {
+        console.log(singleRound(computerSelection(), playerSelection()));
     }
+    if(playerScore > computerScore) return "Human Wins Round of 5!";
+    else return "Computer Wins Round of 5!";
 }
-
-game();
